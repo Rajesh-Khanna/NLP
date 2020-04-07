796664a6d048513f74792be50ab56b22ebec723f
Rajesh Khanna <chrajeshkhanna82798@gmail.com>
2020-04-07 19:55:17 +0530

    Update nlp_helper.py

--- nlp_helper.py
+++ nlp_helper.py
@@ -21,9 +21,9 @@ Word\t POS Tag\t syntactic chunk tag\t named entity tag\t line number
 The data contains multiple lines given in the line number column.
 '''
     print(data_format)
-    os.system('!wget -nc "https://github.com/davidsbatista/NER-datasets/raw/master/CONLL2003/test.txt" -O "test.txt" ')
-    os.system('!wget -nc "https://github.com/davidsbatista/NER-datasets/raw/master/CONLL2003/train.txt" -O "train.txt" ')
-    os.system('!wget -nc "https://github.com/davidsbatista/NER-datasets/raw/master/CONLL2003/valid.txt" -O "valid.txt" ')
+    os.system('wget -nc "https://github.com/davidsbatista/NER-datasets/raw/master/CONLL2003/test.txt" -O "test.txt" ')
+    os.system('wget -nc "https://github.com/davidsbatista/NER-datasets/raw/master/CONLL2003/train.txt" -O "train.txt" ')
+    os.system('wget -nc "https://github.com/davidsbatista/NER-datasets/raw/master/CONLL2003/valid.txt" -O "valid.txt" ')
 
     with open ('test.txt') as f:
         next(f)
